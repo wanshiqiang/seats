70cc63fe50ae99a5f4ce763ee28d037dbda44b03
wanshiqiang <942314578@qq.com>
2019-10-03 23:12:01 +0800

    使用iview

--- src/components/Seat.vue
+++ src/components/Seat.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
-      <img alt="Vue logo" src="../assets/logo.png">
+      <!-- <img alt="Vue logo" src="../assets/logo.png">
       <b>{{name}}</b>
       <b>{{score}}</b>
+      <Button type="primary">Primary</Button>
+      <Button type="dashed">Dashed</Button>
+      <Button type="text">Text</Button>
+      <Button type="info">Info</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="error">Error</Button> -->
+      <br><br>
+      <Card style="width:200px">
+        <p slot="title">
+            <Icon type="ios-school"></Icon>
+            学生1
+        </p>
+
+        <Avatar icon="ios-person" size="large" />
+        <span>得分：67</span>
+        <!-- <span>
+            总分：234
+        </span> -->
+    </Card>
   </div>
 </template>
+  
 
 <script>
 export default {
